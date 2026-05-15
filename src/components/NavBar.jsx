71db996552385129fa95190f86716ffd35bc7e8b
rrrakesh369 <rakesh264743@gmail.com>
2026-05-15 19:08:49 +0530

    Nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
         <li><a>Item 3</a></li> */}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">tracker-certification</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
